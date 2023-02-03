3c6af65f60bd06e5c8f05e0efd59254c27585dd6
Erik Mattfolk <erik.mattfolk@gmail.com>
2023-02-03 20:30:32 +0100

    Add gitignore

--- main.lua
+++ main.lua
@@ -1,3 +1,6 @@
+player = { x = 0, y = 0 }
+
 function love.draw()
-    love.graphics.print('Hello World!', 400, 300)
+	love.graphics.print("Hello World!", 400, 300)
+	love.graphics.rectangle("fill", 0, 0, 100, 100)
 end
