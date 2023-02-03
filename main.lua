8b48c5a0510816b13320bcd98be1a110b54d20ff
Erik Mattfolk <erik.mattfolk@gmail.com>
2023-02-03 21:36:05 +0100

    Add sample dialog

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 local isDown = love.keyboard.isDown
 local player = nil
 local npcs = nil
+local dialog = nil
 
 function createPlayer()
 	return {
@@ -57,18 +58,29 @@ function createNpc(x, y)
 	}
 end
 
-function love.draw()
-	love.graphics.print("Hello World!", 400, 300)
-	player:draw()
-
-	table.foreach(npcs, function(_, npc)
-		npc:draw()
-	end)
+function createDialog(text)
+	return {
+		draw = function(dialog)
+			local dialogHeight = love.graphics.getHeight() / 4
+			local dialogWidth = love.graphics.getWidth()
+			local dialogY = love.graphics.getHeight() - dialogHeight
+			local padding = love.graphics.getHeight() / 20
+			local npcStart = love.graphics.getWidth() * 3 / 4
+			local scale = 2
+			love.graphics.setColor(0.5, 0.5, 0.5)
+			love.graphics.rectangle("fill", 0, dialogY, dialogWidth, dialogHeight)
+			love.graphics.setColor(1, 1, 1)
+			love.graphics.printf(text, padding, dialogY + padding, (npcStart - padding) / scale, "left", 0, scale)
+		end,
+	}
 end
 
 function restart()
 	player = createPlayer()
-	npcs = { createNpc(700, 500), createNpc(400, 500) }
+	npcs = { createNpc(700, 300), createNpc(400, 300) }
+	dialog = createDialog(
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+	)
 end
 
 function love.load()
@@ -85,5 +97,16 @@ function love.update(dt)
 	end
 	if isDown("q") or isDown("escape") then
 		love.event.quit()
+	end
+end
+
+function love.draw()
+	love.graphics.print("Hello World!", 400, 300)
+	table.foreach(npcs, function(_, npc)
+		npc:draw()
+	end)
+	player:draw()
+	if dialog ~= nil then
+		dialog:draw()
 	end
 end
